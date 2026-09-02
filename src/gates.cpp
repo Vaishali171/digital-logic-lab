3592e3ff14455de0c8c19f78614c152bb55ea468
Vaishali171 <24bec110@nith.ac.in>
2026-09-02 23:57:55 +0530

    Implement basic logic gates in C++

---
+++ src/gates.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std ;
+bool AND(bool a, bool b){
+  return a && b;
+}
+bool OR(bool a, bool b){
+  return a || b;
+}
+bool NOT( bool a ){
+return !a;
+}
+int main(){
+cout<< "AND(1,1) =" <<AND(1,1)<< endl;
+cout<< "AND(1,0) =" <<AND(1,0)<< endl;
+
+cout<< "OR(1,1) =" <<OR(1,1)<< endl;
+cout<< "OR(1,1) =" <<OR(1,1)<< endl;
+
+cout<< "NOT(1,1) =" <<AND(1)<< endl;
+cout<< "NOT(1,1) =" <<AND(0)<< endl;
+  return 0;
+}
