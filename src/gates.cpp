8af18f42f2b96c02d4b3574a5f89127173587135
Vaishali171 <24bec110@nith.ac.in>
2026-09-03 20:53:05 +0530

    Refactor gate functions and main output

--- src/gates.cpp
+++ src/gates.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-using namespace std ;
-bool AND(bool a, bool b){
-  return a && b;
-}
-bool OR(bool a, bool b){
-  return a || b;
-}
-bool NOT( bool a ){
-return !a;
-}
-int main(){
-cout<< "AND(1,1) =" <<AND(1,1)<< endl;
-cout<< "AND(1,0) =" <<AND(1,0)<< endl;
+using namespace std;
 
-cout<< "OR(1,1) =" <<OR(1,1)<< endl;
-cout<< "OR(1,1) =" <<OR(1,1)<< endl;
+bool AND(bool a, bool b) {
+    return a && b;
+}
 
-cout<< "NOT(1,1) =" <<AND(1)<< endl;
-cout<< "NOT(1,1) =" <<AND(0)<< endl;
-  return 0;
+bool OR(bool a, bool b) {
+    return a || b;
+}
+
+bool NOT(bool a) {
+    return !a;
+}
+
+int main() {
+
+    cout << "AND(1,1) = " << AND(1,1) << endl;
+    cout << "AND(1,0) = " << AND(1,0) << endl;
+
+    cout << "OR(1,0) = " << OR(1,0) << endl;
+    cout << "OR(0,0) = " << OR(0,0) << endl;
+
+    cout << "NOT(1) = " << NOT(1) << endl;
+    cout << "NOT(0) = " << NOT(0) << endl;
+
+    return 0;
 }
